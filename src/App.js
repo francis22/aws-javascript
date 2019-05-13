30683bbcd0f7d28f30d55e10c0e1f804c0293e45
aaronwht <awhite@tarvold.com>
2019-05-12 20:05:38 -0700

    Updated placeholder

--- src/App.js
+++ src/App.js
@@ -102,7 +102,7 @@ const App = props => {
                   type="text"
                   onChange={handleChange}
                   value={location}
-                  placeholder="Italy"
+                  placeholder="Greece"
                 />
               </div>
             </div>
